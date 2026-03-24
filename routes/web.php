cadc23a003f44043b2f5d53881219343177a503d
faveroo <gabriel290974@gmail.com>
2026-03-24 14:27:00 -0300

    feat: Adiciona o middleware 'throttle:5,1', que significa 5 tentativas a cada 1 minuto.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::inertia('/', 'auth/Login')->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
     Route::inertia('/register', 'auth/Register')->name('register');
-    Route::post('/register', [AuthController::class, 'store']);
+    Route::post('/register', [AuthController::class, 'store'])->middleware('throttle:5,1');
 });
