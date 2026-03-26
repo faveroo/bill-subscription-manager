c7599a685ccff3e7bb05024579996eb2ad2b8d7b
faveroo <gabriel290974@gmail.com>
2026-03-26 09:39:47 -0300

    feat: create Subscription model

---
+++ app/Models/Subscription.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable('subscription_id', 'user_id', 'name', 'amount', 'billing_cycle', 'next_billing_date')]
+class Subscription extends Model
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
