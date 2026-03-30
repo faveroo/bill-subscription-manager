e02e051880339c637c19d89dfd9ae955ad285c48
faveroo <gabriel290974@gmail.com>
2026-03-30 10:22:38 -0300

    feat: Adiciona o método index() para carregar a View com os dados do banco e também store()  para adicionar a nova assinatura no banco, com as validações necessárias.

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\BillingCycle;
+use App\Models\Subscription;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionController extends Controller
-{
+{   
+    public function index()
+    {
+        $subscriptions = Auth::user()->subscriptions()->with('billingCycle')->get();
+        return inertia('subscriptions/Index', [
+            'subscriptions' => $subscriptions
+        ]);
+    }
     public function create()
     {
         return inertia('subscriptions/New', [
@@ -16,6 +25,24 @@ class SubscriptionController extends Controller
 
     public function store(Request $request)
     {
-        return dd($request->all());
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0'],
+            'billing_cycle_id' => ['required', 'exists:billing_cycles,id'],
+            'last_billing' => ['required', 'date'],
+        ]);
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $subscription = $user->subscriptions()->create($data);
+        return redirect()->route('subscriptions.show', $subscription->id)->with('success', 'Assinatura criada com sucesso!');
+    }
+
+    public function show($id)
+    {
+            /** @var \App\Models\User $user */
+            $user = Auth::user();
+            $subscription = $user->subscriptions()->with('billingCycle')->findOrFail($id);
+            dd($subscription);
     }
 }
