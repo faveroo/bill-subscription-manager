1a65219882fe6d3ca03468c5bb1b921d6ef0b2db
faveroo <gabriel290974@gmail.com>
2026-03-30 17:37:41 -0300

    feat: Adiciona botões de ação para as assinaturas do usuário

--- resources/js/pages/subscriptions/Index.tsx
+++ resources/js/pages/subscriptions/Index.tsx
@@ -2,7 +2,7 @@ import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types/pages/subscriptions";
 import MainLayout from "../../layouts/MainLayout";
 import { ReactNode } from "react";
-import { usePage } from "@inertiajs/react";
+import { usePage, Link } from "@inertiajs/react";
 
 export default function Assinaturas() {
     const { props } = usePage<PageProps>();
@@ -17,12 +17,29 @@ export default function Assinaturas() {
                 ) : (
                     <ul className="space-y-4">
                         {subscriptions.map((subscription) => (
-                            <li key={subscription.id} className="bg-gray-800 p-4 rounded-lg">
-                                <h2 className="text-xl text-white font-bold">{subscription.name}</h2>
-                                <p className="text-gray-400">Preço: R$ {subscription.price}</p>
-                                <p className="text-gray-400">Ciclo de cobrança: {subscription.billingCycle?.name}</p>
+                            <li key={subscription.id} className="bg-zinc-800 flex flex-col p-4 rounded-lg">
+                                <div>
+                                    <Link 
+                                        className="text-xl text-white font-bold cursor-pointer"
+                                        href={`/subscriptions/${subscription.id}`}
+                                    >{subscription.name}</Link>
+                                    <p className="text-gray-400">Preço: R$ {subscription.price}</p>
+                                    <p className="text-gray-400">Ciclo de cobrança: {subscription.billing_cycle?.name}</p>
+                                    <p className="text-gray-400">Próxima cobrança: {subscription.next_billing_date}</p>
+                                </div>
+                                <div className="mt-4 pt-3 border-t border-zinc-700 flex justify-end gap-2">
+                                    <button className="border border-blue-600 w-full text-white px-3 py-1 rounded cursor-pointer hover:bg-blue-600 transition-colors">
+                                        Editar
+                                    </button>
+
+                                    <button className="border border-red-600 w-full text-white px-3 py-1 rounded cursor-pointer hover:bg-red-600 transition-colors">
+                                        Excluir
+                                    </button>
+                                </div>
                             </li>
                         ))}
+
+                        
                     </ul>
                 )}
         </>
