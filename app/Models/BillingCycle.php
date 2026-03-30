da5ed83f23136a037445f6ba0e72c1d6f744234a
faveroo <gabriel290974@gmail.com>
2026-03-30 10:19:37 -0300

    chore(relation): Adiciona relacionamento 'HasMany' entre BillingCycle e subscriptions

--- app/Models/BillingCycle.php
+++ app/Models/BillingCycle.php
@@ -8,5 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 #[Fillable(['name', 'interval_days'])]
 class BillingCycle extends Model
 {
-    //
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class, 'billing_cycle_id');
+    }
 }
