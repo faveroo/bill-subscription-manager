91feef14c638127b65dc2c24819ba227f062680d
faveroo <gabriel290974@gmail.com>
2026-04-01 16:16:58 -0300

    feat: adiciona nova tipagem para Subscription, para tratar uma unica assinatura ao inves de várias

--- resources/js/types/model/subscription.ts
+++ resources/js/types/model/subscription.ts
@@ -1,9 +1,10 @@
-import { BillingCycle } from "./billingCycle";
+import { BillingCycle } from './billingCycle';
 
 export type Subscription = {
     id: number;
     name: string;
     price: number;
+    last_billing?: string;
     next_billing_date: string;
     billing_cycle?: BillingCycle;
 };
--- resources/js/types/pages/subscriptions.ts
+++ resources/js/types/pages/subscriptions.ts
@@ -3,3 +3,7 @@ import { Subscription } from "../model/subscription";
 export type PageProps = {
     subscriptions?: Subscription[];
 };
+
+export type SubscriptionInfoProps = PageProps & {
+    subscription: Subscription;
+};
