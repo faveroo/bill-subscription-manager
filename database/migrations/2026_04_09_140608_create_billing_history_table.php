fbcf20524030338ffe887d8306eb757529c77695
faveroo <gabriel290974@gmail.com>
2026-04-09 11:19:36 -0300

    feat: create migration for billing_history table

---
+++ database/migrations/2026_04_09_140608_create_billing_history_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billing_history', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscription_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('billing_date');
+            $table->decimal('amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billing_history');
+    }
+};
