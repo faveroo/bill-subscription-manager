e8f882390e29147b1465d1fd68e94437a77439de
faveroo <gabriel290974@gmail.com>
2026-04-13 10:13:09 -0300

    feat: adiciona created_at e updated_at no compartilhamento do inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -44,6 +44,8 @@ class HandleInertiaRequests extends Middleware
                     'id' => $request->user()->id,
                     'name' => $request->user()->name,
                     'email' => $request->user()->email,
+                    'created_at' => $request->user()->created_at,
+                    'updated_at' => $request->user()->updated_at,
                 ]
                 : null,
             ],
