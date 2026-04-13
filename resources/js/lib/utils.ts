5c527917eb16f30380067cb4a9e467810f591a59
faveroo <gabriel290974@gmail.com>
2026-04-13 10:13:31 -0300

    refactor: adiciona format date como uma função global utilitária

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -5,3 +5,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function formatDate(date?: string) {
+        if (!date) return '';
+        return new Date(date).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+        });
+    };
