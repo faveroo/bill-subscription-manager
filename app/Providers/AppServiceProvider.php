b90cf5719cb51a04b2448ecac14547a26fb39df0
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-04-16 22:24:45 -0300

    Force HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+         if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         Inertia::share([
             'auth' => [
                 'user' => fn() => Auth::user(),
